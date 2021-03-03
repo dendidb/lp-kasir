d41654cb28c69a4ce78745134d243a6dcf863d1d
Dendi Budiansyah <budidendi1234@gmail.com>
2021-03-03 16:14:05 +0700

    sale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("navbar-sticky")
-  } else {
-    navbar.classList.remove("navbar-sticky");
-  }
-}
-
-// Menu mobile
-
-if ($(".mobile-menu").length) {
-  $(".mobile-menu").on("click", function() {
-    $(this).toggleClass("active");
-    $(".nav-menu").slideToggle(300);
-  });
-}
-
 $('.owl-carousel').owlCarousel({
   stagePadding: 50,
   loop:true,
@@ -48,51 +26,44 @@ $('.owl-carousel').owlCarousel({
   }
 })
 
-$('.service-carousel').owlCarousel({
-  loop:true,
-  autoplay:true,
-  lazyLoad: true,
-  autoplayTimeout:1000,
-  smartSpeed: 2000,
-  autoplayHoverPause:true,
-  nav:true,
-  dots:true,
-  responsiveClass:true,
-  responsive:{
-    0:{
-      items:1,
-      margin: 20
-    },
-    600:{
-      items:2,
-      margin: 20
-    },
-    1000:{
-      items:3,
-    }
-  }
-})
+// $('.service-carousel').owlCarousel({
+//   loop:true,
+//   autoplay:true,
+//   lazyLoad: true,
+//   autoplayTimeout:1000,
+//   smartSpeed: 2000,
+//   autoplayHoverPause:true,
+//   nav:true,
+//   dots:true,
+//   responsiveClass:true,
+//   responsive:{
+//     0:{
+//       items:1,
+//       margin: 20
+//     },
+//     600:{
+//       items:2,
+//       margin: 20
+//     },
+//     1000:{
+//       items:3,
+//     }
+//   }
+// })
 
-$('.banner-carousel').owlCarousel({
-  loop:true,
-  autoplay:true,
-  lazyLoad: true,
-  autoplayTimeout:1000,
-  smartSpeed: 2000,
-  autoplayHoverPause:true,
-  dots:true,
-  responsiveClass:true,
-  responsive:{
-    0:{
-      items:1,
-      margin: 20
-    }
-  }
-})
-
-const dropdown = document.querySelector('.dropdown');
-const dropdownMenu = document.querySelector('.dropdown-menu')
-dropdown.addEventListener('click',()=>{
-  dropdownMenu.classList.toggle('active')
-})
-
+// $('.banner-carousel').owlCarousel({
+//   loop:true,
+//   autoplay:true,
+//   lazyLoad: true,
+//   autoplayTimeout:1000,
+//   smartSpeed: 2000,
+//   autoplayHoverPause:true,
+//   dots:true,
+//   responsiveClass:true,
+//   responsive:{
+//     0:{
+//       items:1,
+//       margin: 20
+//     }
+//   }
+// })
