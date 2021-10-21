e0a7f58729ee3a482b8faf0c30bceaba65812e3b
Dendi Budiansyah <budidendi1234@gmail.com>
2021-10-21 14:05:58 +0700

    finishing web

--- data.php
+++ data.php
@@ -16,5 +16,5 @@ $_SESSION["jenis_usaha"] = $company_type;
 // $_SESSION["email"] = $email;
 $_SESSION["alamat"] = $address;
 
-header('Location: https://api.whatsapp.com/send?phone=6285864772042&text=Hallo Admin.%0ASaya%20ingin%20bertanya%20mengenai%20Aplikasi%20Kasir%20RZF%20Software!%0A%0ANama%20%3A%20'.$_SESSION["nama"].'%0AProduk%20%3A%20Aplikasi%20Kasir%0A%3A%20%0AJenis%20Usaha%20%3A%20'.$_SESSION["jenis_usaha"].'%0AAlamat%20%3A%20'.$_SESSION["alamat"]);
+header('Location: https://api.whatsapp.com/send?phone=6285864772042&text=Hallo Admin.%0ASaya%20ingin%20memesan%20Aplikasi%20Kasir%20RZF%20Software!%0A%0ANama%20%3A%20'.$_SESSION["nama"].'%0AProduk%20%3A%20Aplikasi%20Kasir%20%0AJenis%20Usaha%20%3A%20'.$_SESSION["jenis_usaha"].'%0AAlamat%20%3A%20'.$_SESSION["alamat"]);
 
